88cee9adf38c0e6c2fd068bf536dcb74379b4557
RyanCross <StudioPod.RyanCross@outlook.com>
2018-08-11 16:04:44 -0500

    added simple track generation

---
+++ Assets/Prefabs/Track/TrackGenerationSetup.md
@@ -0,0 +1,12 @@
+﻿## Setting Up Track Generation for a Scene
+
+Ensure the TrackGenerator and RaceTrack prefabs have been placed in the Scene.
+
+In the TrackGeneratorPrefab, ensure that:
+
+ - The size of Track Segment Prefabs is at least 1.
+ - Ensure that the StraightTrack01 prefab has been added to 		  the track segment prefabs.
+
+
+
+> Written with [StackEdit](https://stackedit.io/).
---
+++ Assets/Scripts/RaceTrackController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceTrackController : MonoBehaviour {
+
+    // placeholder script for the class the holds the current state of the track.
+    // Retrieves new TrackSegment information from the TrackGeneratorController.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
