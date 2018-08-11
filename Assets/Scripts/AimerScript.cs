5291ba73c06c49804ec446f8b4e419f4ab925f53
cole-c <cole.e.cummings@gmail.com>
2018-08-11 15:09:11 -0500

    Player controls using physics/torque initial commit

---
+++ Assets/Scripts/AimerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimerScript : MonoBehaviour {
+
+    GameObject HamBall;
+    GameObject Aimer;
+
+	// Use this for initialization
+	void Start () 
+    {
+        HamBall = GameObject.Find("HamsterBall");
+        Aimer = GameObject.Find("Aimer");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        Aimer.transform.position = new Vector3(HamBall.transform.position.x - 3, HamBall.transform.position.y, HamBall.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/HamsterSphere.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HamsterSphere : MonoBehaviour {
+    
+    float speed    = 10.0f;
+    float distance = 10.0f;
+    float height   = 10.0f;
+    float damping  = 2.0f;
+
+    void Start()
+    {
+        if (!GetComponent<Rigidbody>())
+            gameObject.AddComponent(typeof(Rigidbody));
+
+        // setup camera
+        var trans = Camera.main.transform;
+        var wantedPosition = new Vector3(0, height, distance);
+        wantedPosition += transform.position;
+        trans.position = wantedPosition;
+        trans.LookAt(transform);
+    }
+
+    void FixedUpdate()
+    {
+        var move = new Vector3(Input.GetAxis("Vertical"), 0, -Input.GetAxis("Horizontal"));
+        if (move.magnitude > 0.0)
+        {
+            var y = Camera.main.transform.position.y;
+            //Keep camera height the same in relation to ball height
+            Vector3 position = Camera.main.transform.position;
+            position.y = transform.position.y;
+            transform.position = position;
+
+            move = Camera.main.transform.TransformDirection(move.normalized);
+
+            gameObject.GetComponent<Rigidbody>().AddTorque(move * speed);
+
+            position = Camera.main.transform.position;
+            position.y = transform.position.y;
+            transform.position = position;
+        }
+    }
+
+    void LateUpdate()
+    {
+        var trans = Camera.main.transform;
+
+        var wantedPosition = -gameObject.GetComponent<Rigidbody>().velocity;
+
+        wantedPosition.y = 0;
+        wantedPosition = wantedPosition.normalized * distance;
+        wantedPosition.y = height;
+        wantedPosition += transform.position;
+        trans.position = Vector3.Lerp(trans.position, wantedPosition, damping * Time.deltaTime);
+        trans.LookAt(transform);
+    }
+}
