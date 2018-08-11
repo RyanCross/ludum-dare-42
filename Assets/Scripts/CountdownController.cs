b792b70a8c3c366892eb4aca57e3e98c8090c5a5
Alisha Parvez <aparvez@usc.edu>
2018-08-11 14:19:56 -0500

    Countdown Script created

---
+++ Assets/Scripts/CountdownController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownController : MonoBehaviour
+{
+
+    public int timeLeft = 3;
+    public Text countdown;
+    private float waitSeconds = 0.5f;
+
+    void Start()
+    {
+        StartCoroutine("StartCountdown");
+    }
+
+    void Update()
+    {
+        if(timeLeft == 0)
+        {
+            countdown.text = ("Go!");
+        }
+        else
+        {
+            countdown.text = ("" + timeLeft);
+        }
+    }
+    
+    IEnumerator StartCountdown()
+    {
+        while (timeLeft > 0)
+        {
+            yield return new WaitForSeconds(waitSeconds);
+            timeLeft--;
+        }
+
+    }
+}
