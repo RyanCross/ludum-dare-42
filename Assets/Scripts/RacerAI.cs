8654af1ff1d3066f9c2fb5144d1da0e93824dc29
Matt Miller <matmille@microsoft.com>
2018-08-10 21:05:44 -0700

    Initial Enemy AI and IRacer Interface.

---
+++ Assets/Scripts/RacerAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacerAI : MonoBehaviour, IRacer {
+
+    // Needs to be set at the start of the race to the first waypoint, probably.
+    // i.e., drag first waypoint here in the inspector
+    private int _currentWaypoint;
+
+    public bool Dead { get; set; } = false;
+
+    private void Start()
+    {
+
+    }
+
+    public void Die()
+    {
+        Dead = true;
+        Debug.Log(this.ToString() + " died!");
+
+        // Some other stuff
+    }
+    
+    // Probably will remove this from the scene, but for now...
+    public void Move()
+    {
+        if (Dead) return;
+        Debug.Log(this.ToString() + " is moving towards " + _currentWaypoint.ToString());
+    }
+    
+    // Do nothing, for now
+    public void Win()
+    {    
+        Debug.Log(this.ToString() + " died!");
+    }
+
+    // We entered a new zone for a waypoint. Go towards the next one
+    public void ModifyTargetWaypoint(int number)
+    {
+        _currentWaypoint = number;
+    }
+}
---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour {
+
+    public int Number { get; } = -1;
+
+    public Waypoint(int number)
+    {
+        Number = number;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("AI"))
+        {
+            other.GetComponent<RacerAI>().ModifyTargetWaypoint(Number);
+        }
+    }
+}
