9cab14e1ff87892968aa5df44e26408e9ba94dce
Matt Miller <matmille@microsoft.com>
2018-08-11 09:23:23 -0700

    Basic enemy waypoint system.

--- Assets/Scripts/RacerAI.cs
+++ Assets/Scripts/RacerAI.cs
@@ -6,13 +6,25 @@ public class RacerAI : MonoBehaviour, IRacer {
 
     // Needs to be set at the start of the race to the first waypoint, probably.
     // i.e., drag first waypoint here in the inspector
-    private int _currentWaypoint;
+    public Waypoint _currentWaypoint;
 
     public bool Dead { get; set; } = false;
+    public float speed = 0.3f;
+    public float wobbliness = .2f;
+    public float waypointInaccuracy = 2.0f;
 
     private void Start()
     {
 
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        if(Random.value < wobbliness)
+        {
+            ResetRotation();
+        }
     }
 
     public void Die()
@@ -28,6 +40,8 @@ public class RacerAI : MonoBehaviour, IRacer {
     {
         if (Dead) return;
         Debug.Log(this.ToString() + " is moving towards " + _currentWaypoint.ToString());
+
+        gameObject.transform.Translate(0f, 0f, speed);
     }
     
     // Do nothing, for now
@@ -37,8 +51,26 @@ public class RacerAI : MonoBehaviour, IRacer {
     }
 
     // We entered a new zone for a waypoint. Go towards the next one
-    public void ModifyTargetWaypoint(int number)
+    public void ModifyTargetWaypoint(Waypoint waypoint)
     {
-        _currentWaypoint = number;
+        _currentWaypoint = waypoint;
+        ResetRotation();
+    }
+
+    private void ResetRotation()
+    {
+        Vector3 fuzziness = GenerateFuzziness(0.0f, waypointInaccuracy);
+        gameObject.transform.LookAt(_currentWaypoint.transform.position + fuzziness);
+    }
+
+    // We don't want all the enemy race boys bumping into each other.
+    private Vector3 GenerateFuzziness(float min = 0.0f, float max = 1.0f)
+    {
+        return new Vector3()
+        {
+            x = Random.Range(min, max),
+            y = 0f,
+            z = Random.Range(min, max)
+        };
     }
 }
