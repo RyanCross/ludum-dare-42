43afdb7da3cd3886510c910fc0cbd449e1313a3d
Ryan Cross <StudioPod.RyanCross@outlook.com>
2018-08-11 16:23:29 -0500

    added simple track generation (#11)

---
+++ Assets/Scripts/TrackSegment.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// telling unity that this class is Serializable will allow it to show up in the inspector
+[System.Serializable]
+public class TrackSegment
+{
+    public string trackName;
+    public GameObject trackSegmentPrefab;
+}
