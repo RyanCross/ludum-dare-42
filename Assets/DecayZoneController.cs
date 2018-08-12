83d7516c92abee7fec2608ae9de5fcbc0301a4d3
RyanCross <StudioPod.RyanCross@outlook.com>
2018-08-12 13:32:05 -0500

    REFACTORED ALL THE THINGS

---
+++ Assets/DecayZoneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecayZoneController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            RaceTrackController raceTrackController = GameObject.FindGameObjectWithTag("RaceTrack").GetComponent<RaceTrackController>();
+
+            //decay the first piece of the track in the list
+        }
+    }
+
+}
