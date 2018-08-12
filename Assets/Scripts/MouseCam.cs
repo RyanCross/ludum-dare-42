f61ded534c07dd7d9795ef80ffe6ad20de8ddfa1
RyanCross <StudioPod.RyanCross@outlook.com>
2018-08-12 15:12:54 -0500

    Fixed merge conflicts

--- Assets/Scripts/MouseCam.cs
+++ Assets/Scripts/MouseCam.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseCam : MonoBehaviour {
+public class MouseCam : MonoBehaviour
+{
 
-    private const float Y_ANGLE_MIN = 90.0f;
-    private const float Y_ANGLE_MAX = 170.0f;
+    private const float Y_ANGLE_MIN = 0.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
     public Transform lookAt;
-    public Transform camTransform;
+    private Transform camTransform;
 
     private Camera cam;
 
     private float distance = 10.0f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
-    private float sensivityX = 4.0f;
-    private float sensivityY = 1.0f;
+    private float sensivityX = 5.0f;
+    private float sensivityY = 5.0f;
 
 
     // Use this for initialization
@@ -23,6 +24,8 @@ public class MouseCam : MonoBehaviour {
     {
         camTransform = transform;
         cam = Camera.main;
+        //Set Cursor to not be visible
+        Cursor.visible = false;
     }
 
     private void Update()
@@ -36,8 +39,10 @@ public class MouseCam : MonoBehaviour {
     private void LateUpdate()
     {
 
-        Vector3 dir = new Vector3(0, 0 - distance);
-        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        Vector3 dir = new Vector3(0, 0, - distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX * sensivityX, 0);
+        //var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         camTransform.position = lookAt.position + rotation * dir;
         camTransform.LookAt(lookAt.position);
     }
