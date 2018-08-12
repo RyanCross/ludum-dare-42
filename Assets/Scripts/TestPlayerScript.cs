43b82ff75eff6b48b465485d172aafc2fc17d480
warrenscipio <warrenscipio@gmail.com>
2018-08-12 09:40:04 -0500

    asset things

---
+++ Assets/Scripts/TestPlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerScript : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    private void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal") * 2;
+        float z = Input.GetAxis("Vertical") * 2;
+
+        Vector3 movement = new Vector3(x, 0.0f, z);
+        rb.AddForce(movement * 2);
+    }
+}
